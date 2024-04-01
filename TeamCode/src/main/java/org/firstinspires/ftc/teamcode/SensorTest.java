04afda42ee269424f1d39d5c470931d4318479b2
addiment <63932310+addiment@users.noreply.github.com>
2024-04-01 16:26:24 -0400

    Update SensorTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTest.java
@@ -45,6 +45,7 @@ public class SensorTest extends OpMode {
 
         plow.raise();
 
+
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
@@ -66,14 +67,16 @@ public class SensorTest extends OpMode {
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
         // IMU orientation/calibration
-        RevHubOrientationOnRobot.LogoFacingDirection logo = RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
-        RevHubOrientationOnRobot.UsbFacingDirection usb = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot.LogoFacingDirection logo = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
+        RevHubOrientationOnRobot.UsbFacingDirection usb = RevHubOrientationOnRobot.UsbFacingDirection.UP;
         imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(logo, usb)));
-
+        imu.resetYaw();
     }
+
     @Override public void start() {
         imu.resetYaw();
         plow.raise();
+
     }
     @Override public void loop() {
        double frontDistance = (Dist1.getDistance(DistanceUnit.CM));
@@ -82,5 +85,6 @@ public class SensorTest extends OpMode {
        telemetry.addData("Front Distance", frontDistance);
        telemetry.addData("Orientation", rotation);
        telemetry.addData("Yaw", yawRot);
+
     }
 }
